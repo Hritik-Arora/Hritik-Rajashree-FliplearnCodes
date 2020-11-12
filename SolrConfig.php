5934085d73e046a5b4a2e3dd0e1a5bb97037eebd
Hritik-Arora <41345450+Hritik-Arora@users.noreply.github.com>
2020-11-12 21:25:43 +0530

    Update SolrConfig.php

--- SolrConfig.php
+++ SolrConfig.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "http://localhost:8983/solr/FliplearnDoubtsDb/select?q=Mathpix_API_Response%3A\"";
+$url = "url";
         
 $escapeCharacters = array('/','\\','+','-','&&','||','!','(',')','{','}','[',']','^','~','*','?',':');
         
